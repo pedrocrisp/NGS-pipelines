8a68d6fe0a580745b992a60584f0d77c24f1604e
Kevin Murray <spam@kdmurray.id.au>
2014-04-03 17:43:40 +1100

    [getScytheResults.sh] use python log scraper

--- RNAseqPipe3/08-getScytheResults.sh
+++ RNAseqPipe3/08-getScytheResults.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
-
 #  08-getScytheResults.sh
-#  
 #
-#  Created by Peter Crisp on 27/03/2014.
+#
+#  Created by Peter Crisp on 27/03/2014. Mauled to pieces by KDM.
 #
 set -e
 set -x
 
-function findSamples () {
-find logs/reads_noadapt.*/  -mindepth 1 -maxdepth 1 -type f -name Sample* | tr ' ' '\n'
-}
+logscript=$(readlink -f $0)/../tools/scythe_log.py
 
-samples=$(findSamples)
-
-for sample in $samples
-do
-echo $(basename $sample)
-grep contaminated* $sample
-done
+find logs/reads_noadapt.*/  -mindepth 1 -maxdepth 1 -type f -name Sample* | xargs python $logscript >results_summaries/reads_noadapt.csv
