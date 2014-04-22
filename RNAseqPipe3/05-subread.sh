eb5c46f65417b916e7a4b3577b3582ffcb6c4460
BecWardell <rebecca.wardell@anu.edu.au>
2014-04-22 16:08:31 +1000

    Update 05-subread.sh

--- RNAseqPipe3/05-subread.sh
+++ RNAseqPipe3/05-subread.sh
@@ -20,20 +20,20 @@ fi
 
 ###
 #User defined reference sequence directory.
-#This line defines the reference directory to be located in the script directory in a folder called 'subread_refdir.' The user should create a link for the subread_refdir in the script dir to map to the location of the directory containing their subread indexfiles. These index files can be created by using the 'subread-buildindex' program (refer to RNAseq pipeline user guide) and MUST have the prefix "TAIR10_gen_chrc" (chrc means we included all 7 chromosomes).
+#This line specifies that the reference directory is located in the script directory in a folder called 'subread_refdir.' The user should create a link for the subread_refdir in the script dir to map to the location of the directory containing their subread indexfiles. These index files can be created by using the 'subread-buildindex' program (refer to RNAseq pipeline user guide) and MUST have the prefix "TAIR10_gen_chrc" (chrc means we included all 7 chromosomes).
 refdir=$scriptdir/subread_refdir
 #
 
 #Defines the sample that we are working with to the command line as the first token.
 sample=$1
 
-#Specifies the directory that the sample will be opened from.In this case, it is opening the sample folders located in the 'reads_noadapt_trimmed' folder.
+#Specifies the directory that the sample will be opened from. In this case, it is opening a sample folder located in the 'reads_noadapt_trimmed' folder.
 sample_dir=reads_noadapt_trimmed/$sample
 
 #Defines the output directory to be a folder with the sample name located within the 'align' directory. This will be used in the next step to create an output directory. 
 outdir="align/${sample}"
 
-#Creates an output directory to put the returned files to go into once subread has been run on the sample. The output from subread for the sample should go into a folder containing the sample's name, located within the 'align' directory.
+#Creates an output directory to put the returned files to go into once subread has been run on the sample. In this case, the output from subread for the sample should go into a folder containing the sample's name, located within the 'align' directory.
 mkdir ${outdir}
 
 #List all files ending with 'trimmed.fq' that are located within the specified sample directory and save these as the variable 'fastqs.'
