2239962862d26c8021f55fe1bf494969a832e73a
Peter Crisp <pedrocrisp@gmail.com>
2014-06-02 23:53:40 +1000

    03-sickle.sh added* to *R1*

--- RNAseqPipe3/03-sickle.sh
+++ RNAseqPipe3/03-sickle.sh
@@ -18,10 +18,10 @@ sample_dir="${inputDir}/$sample"
 outDir=reads_noadapt_trimmed
 
 #List all files ending with 'R1.noadapt.fq.gz' that are located within the specified sample directory and save these as the variable 'forward_fq.'
-forward_fq="$(ls $sample_dir/*R1.noadapt.fq.gz)"
+forward_fq="$(ls $sample_dir/*R1*.noadapt.fq.gz)"
 
 #List all files ending with R2.noadapt.fq.gz that are located within the specified sample directory and save these as the variable 'reverse_fq.'
-reverse_fq="$(ls $sample_dir/*R2.noadapt.fq.gz)"
+reverse_fq="$(ls $sample_dir/*R2*.noadapt.fq.gz)"
 
 #Create the output directory and within this folder create a directory for the given sample. 
 mkdir "${outDir}/$sample"
