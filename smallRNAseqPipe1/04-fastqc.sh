76b1f5ff84423ce1c0453ebd1d87001c4d4d9c4b
Peter Crisp <pedrocrisp@gmail.com>
2014-06-14 10:23:15 +1000

    added 04-fastqc.sh

---
+++ smallRNAseqPipe1/04-fastqc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+ 
+if [ $# -ne 1 ]
+then
+echo "USAGE: fastqc.sh SAMPLENAME"
+fi
+ 
+sample=$1
+sample_dir=reads_noadapt_trimmed/$sample
+ 
+fastqs="$(ls $sample_dir/*trimmed.fq)"
+ 
+mkdir reads_noadapt_trimmed_fastqc/$sample
+ 
+fastqc -o reads_noadapt_trimmed_fastqc/$sample $fastqs
---
+++ smallRNAseqPipe1/04-runner.sh
@@ -0,0 +1,36 @@
+###
+#code to make it work on osx and linux
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+#user defined variables that could be changed:
+workingdir=./
+script=$scriptdir/04-fastqc.sh
+outdir=reads_noadapt_trimmed_fastqc
+###
+
+function findSamples () {
+find reads_noadapt_trimmed/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
+}
+
+mkdir ${outdir}
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+logdir="./logs/${outdir}.${timestamp}"
+mkdir $logdir
+
+cat $script > "$logdir/script.log"
+cat $0 > "$logdir/runner.log"
+cat $script
+
+findSamples | parallel bash $script {} \>logs/${outdir}.${timestamp}/{}.log 2\>\&1
+
+#To run:
+#bash ~/path_to/01-runner.sh
