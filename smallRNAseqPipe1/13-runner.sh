933cce619fc5aaeb51c9c5ed05d0575ecb26e822
Peter Crisp <pedrocrisp@gmail.com>
2014-06-17 14:20:15 +1000

    alternate 13-cutadapt.sh

---
+++ smallRNAseqPipe1/13-runner.sh
@@ -0,0 +1,37 @@
+###
+#code to make it work on osx and linux
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+#user defined variables that could be changed:
+workingdir=./
+script=$scriptdir/13-cutadapt.sh
+outdir=reads_noadapt
+###
+
+function findSamples () {
+find reads/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
+}
+
+mkdir $outdir
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+logdir="./logs/${outdir}.${timestamp}"
+mkdir $logdir
+
+cat $script > "$logdir/script.log"
+cat $0 > "$logdir/runner.log"
+cat $script
+
+findSamples | parallel bash $script {} \>logs/${outdir}.${timestamp}/{}.log 2\>\&1
+
+#To run, got to directory containing reads directory and call:
+#bash ~/path_to/02-runner.sh
+
