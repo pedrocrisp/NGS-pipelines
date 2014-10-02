642dec6990aa07cbbd67a5ef39be16d47aa0a456
Peter Crisp <pedrocrisp@gmail.com>
2014-10-02 23:29:40 +1000

    added draft tophat scripts

---
+++ RNAseqPipe3/15-runner.sh
@@ -0,0 +1,36 @@
+###
+#code to make it work on osx and linux
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+#user defined variables that could be changed:
+workingdir=./
+script=$scriptdir/05-tophat2.sh
+###
+
+function findSamples () {
+find reads_noadapt_trimmed/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
+}
+
+outdir=align
+mkdir ${outdir}
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+logdir="./logs/${outdir}_tophat.${timestamp}"
+mkdir $logdir
+
+cat $script > "$logdir/script.log"
+cat $0 > "$logdir/runner.log"
+cat $script
+
+findSamples | parallel bash $script {} \>logs/${outdir}_tophat.${timestamp}/{}.log 2\>\&1
+
+#To run:
+#bash ~/path_to/05-runner.sh
