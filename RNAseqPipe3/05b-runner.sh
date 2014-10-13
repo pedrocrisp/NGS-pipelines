a9c3a77c232c265097e3f58b75d07eba585a1a4b
Peter Crisp <pedrocrisp@gmail.com>
2014-10-14 03:51:25 +1100

    05b-samtools split from subread script

--- RNAseqPipe3/05b-runner.sh
+++ RNAseqPipe3/05b-runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  05-runner.sh
+#  05b-runner.sh
 #  subread runner script
 #
 #  Created by Peter Crisp on 14/10/2014.
@@ -15,39 +15,28 @@ set -x
 #getops
 unset name
 jflag=
-Pflag=
-while getopts j:P: name
+while getopts j: name
 do
     case $name in
         j)    jflag=1
         jval="$OPTARG";;
-        P)    Pflag=1
-        Pval="$OPTARG";;
         ?)   printf "Usage: %s: [-j value] [-P value] args\n" $0
-            exit 1;;
+        exit 1;;
         *)   printf "Usage: %s: [-j value] [-P value] args\n" $0
-            exit 1;;
+        exit 1;;
     esac
 done
 if [ ! -z "$jflag" ]; then
     printf 'Parallel will use -j "%s" threads\n' "$jval"
 fi
-if [ ! -z "$Pflag" ]; then
-    printf 'sunbread will use -P "%s" threads\n' "$Pval"
-fi
 shift $(($OPTIND - 1))
 
 if [ -z "$jflag" ]
 then
-printf "Threads for parallel not specified\n Usage: %s: [-j value] [-P value] Threads for parallel not specified\n" $0
+    printf "Threads for parallel not specified\n Usage: %s: [-j value] [-P value] Threads for parallel not specified\n" $0
 exit
 fi
 
-if [ -z "$Pflag" ]
-then
-printf "Threads for subread not specified\n Usage: %s: [-j value] [-P value] args\n" $0
-exit
-fi
 
 
 ###find script directory (for purpose of locating target scripts and reference sequences
@@ -62,15 +51,15 @@ scriptdir="$(dirname $(readlink -f $0))"
 fi
 #set script dir and working dir
 workingdir=./
-script=$scriptdir/05-subread.sh
+script=$scriptdir/05b-samtools.sh
 ###
 
 function findSamples () {
-find reads_noadapt_trimmed/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
+find align/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
 }
 
 outdir=align
-mkdir ${outdir}
+#mkdir ${outdir}
 timestamp=$(date +%Y%m%d-%H%M%S)
 
 logdir="./logs/${outdir}_subread.${timestamp}"
@@ -80,7 +69,7 @@ cat $script > "$logdir/script.log"
 cat $0 > "$logdir/runner.log"
 cat $script
 
-findSamples | parallel -j $jval bash $script {} $Pval \>logs/${outdir}_subread.${timestamp}/{}.log 2\>\&1
+findSamples | parallel -j $jval bash $script {} \>$logdir/{}.log 2\>\&1
 
 #To run:
-#bash ~/path_to/05-runner.sh
+#bash ~/path_to/05b-runner.sh
