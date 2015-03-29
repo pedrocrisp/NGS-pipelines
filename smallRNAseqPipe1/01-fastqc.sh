e7168fefe91d3eb1cca3b3d9bf8e610c405c7a74
Peter Crisp <pedrocrisp@gmail.com>
2015-03-29 22:17:00 +1100

    Replaced sRNA 01-* with PARE 01-* using links

--- smallRNAseqPipe1/01-fastqc.sh
+++ smallRNAseqPipe1/01-fastqc.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-set -e
-set -x
- 
-if [ $# -ne 1 ]
-then
-echo "USAGE: fastqc.sh SAMPLENAME"
-fi
- 
-sample=$1
-sample_dir=reads/$sample
- 
-fastqs="$(ls $sample_dir/*.fastq.gz)"
- 
-mkdir reads_fastqc/$sample
- 
-fastqc -o reads_fastqc/$sample $fastqs
+../PARE_pipe1/01-fastqc.sh
