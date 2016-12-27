4819a9198b6a877190c37d0ff8106a3b260874eb
pedrocrisp <pedrocrisp@users.noreply.github.com>
2016-12-27 22:14:19 +1100

    Update MakeGeneCoverageBeds.sh

--- GeneCoverage/MakeGeneCoverageBeds.sh
+++ GeneCoverage/MakeGeneCoverageBeds.sh
@@ -28,13 +28,13 @@ mkdir ${outFolder}
 
 if [ "$coverage" == "5prime" ]
 then
-#make a real bed file by adding chromosome edn co-ordinates (same as start because nt resolution)
+#make a real bed file by adding chromosome end co-ordinates (same as start because nt resolution)
 awk 'BEGIN { FS="\t"; OFS="\t" } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.plus5.bed > $outFolder/$sample.plus.real.bed
 awk 'BEGIN { FS="\t"; OFS="\t" } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.minus5.bed > $outFolder/$sample.minus.real.bed
 
 elif [ "$coverage" == "full" ]
 then
-#make a real bed file by adding chromosome edn co-ordinates (same as start because nt resolution)
+#make a real bed file by adding chromosome end co-ordinates (same as start because nt resolution)
 awk 'BEGIN { FS="\t"; OFS="\t" } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.plus.bed > $outFolder/$sample.plus.real.bed
 awk 'BEGIN { FS="\t"; OFS="\t" } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.minus.bed > $outFolder/$sample.minus.real.bed
 
@@ -44,13 +44,17 @@ exit 1
 fi
 
 #sort
-sort -k1,1 -k2,2n $outFolder/$sample.plus.real.bed > $outFolder/$sample.plus.real.bed
-sort -k1,1 -k2,2n $outFolder/$sample.minus.real.bed > $outFolder/$sample.minus.real.bed
+sort -k1,1 -k2,2n $outFolder/$sample.plus.real.bed > $outFolder/$sample.plus_sorted.bed
+sort -k1,1 -k2,2n $outFolder/$sample.minus.real.bed > $outFolder/$sample.minus_sorted.bed
+#remove intermediate file
+rm -rv $outFolder/*real.bed
 
 #get distance measures
 #reference is TAIR10_crisp_gene_primary.bed - fancy exon bed file prepared by SRE
-closestBed -D "ref" -a $outFolder/$sample.plus.real.bed -b $reference > $outFolder/$sample.plus.dist.bed
-closestBed -D "ref" -a $outFolder/$sample.minus.real.bed -b $reference > $outFolder/$sample.minus.dist.bed
+closestBed -D "ref" -a $outFolder/$sample.plus_sorted.bed -b $reference > $outFolder/$sample.plus.dist.bed
+closestBed -D "ref" -a $outFolder/$sample.minus_sorted.bed -b $reference > $outFolder/$sample.minus.dist.bed
+#remove intermediate file
+rm -rv $outFolder/*_sorted.bed
 
 #subset to 1kb
 awk -F$'\t' '$NF<1000 && $NF>-1000' $outFolder/$sample.plus.dist.bed > $outFolder/$sample.plus.dist.1k.bed
@@ -58,7 +62,7 @@ awk -F$'\t' '$NF<1000 && $NF>-1000' $outFolder/$sample.minus.dist.bed > $outFold
 
 # Then remove any files absolutely required from making the plot, Just keep the dist.1k.bed
 rm -rv $outFolder/*dist.bed
-rm -rv $outFolder/*real.bed
+
 
 
 
