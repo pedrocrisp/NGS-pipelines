bab927b551bac01ac197501980ba5b527023ebc4
pedrocrisp <pedrocrisp@gmail.com>
2017-08-16 11:15:06 -0500

    gro-seq module

--- PARE_pipe1/05-bowtie2.sh
+++ PARE_pipe1/05-bowtie2.sh
@@ -3,7 +3,7 @@
 #Set -e as an option, it tells the command line to exit the script immediately if it registers an error.
 set -e
 
-#Set -x as an option, it tells the computer to echo back each step before it is completed and the output is produced. 
+#Set -x as an option, it tells the computer to echo back each step before it is completed and the output is produced.
 set -x
 
 ###
@@ -36,7 +36,7 @@ multimapping=$5
 #Specifies the directory that the sample will be opened from. In this case, it is opening a sample folder located in the 'reads_noadapt_trimmed' folder.
 sample_dir="$reads/$sample"
 
-#Defines the output directory to be a folder with the sample name located within the 'align' directory. This will be used in the next step to create an output directory. 
+#Defines the output directory to be a folder with the sample name located within the 'align' directory. This will be used in the next step to create an output directory.
 outdir="align_bowtie2/${sample}"
 
 #Creates an output directory to put the returned files to go into once subread has been run on the sample. In this case, the output from subread for the sample should go into a folder containing the sample's name, located within the 'align' directory.
@@ -51,7 +51,7 @@ numFqFiles=$(echo $fastqs | wc -w)
 #Specifies that the sam output file will be placed in the output directory, and have the file name 'sample.sam'
 outsam="${outdir}/${sample}.sam"
 
-#Specifies that the bam output file will be stored in the output directory, with the file name 'sample.''.bam' has not been added as samtools sort -f currently (29/4/14) has a bug. 
+#Specifies that the bam output file will be stored in the output directory, with the file name 'sample.''.bam' has not been added as samtools sort -f currently (29/4/14) has a bug.
 outbam="${outdir}/${sample}.bam" # no .bam, as samtools sort -f has a bug.
 
 #Specifies that the temporary bam output file will be stored in the output directory with the file name 'random.bam.' A temporary bam file has been created due to samtools having a bug with the bam files (Kevins hackery).
@@ -90,6 +90,37 @@ bowtie2 \
 # --score-min C,0,0 would tell bowtie2 to report only exact matches in --end-to-end mode (alignment score of 0 required which is max possible in end mode)
 #-N max # mismatches in seed alignment; can be 0 or 1 (0)
 #-D give up extending after <int> failed extends in a row (15)
+## this combo of -DRNLi is the same as the preset --very-sensitive except L is shorter
+
+if [ "$multimapping" == "groseq_multi_1" ]
+then
+
+bowtie2 \
+-x $reference \
+--phred33 \
+--mm \
+-k 2 \
+--very-sensitive-local \
+-p $b_threads \
+-U $fastqs \
+-S "$outsam"
+
+#-a report all mapping locations
+#-x bowtie index
+#--phred33
+#--very-sensitive-local allow soft trimming of read ends and try harder and take more time for better alignment
+#--mm memory-mapped I/O to load index allow multiple processes to use index
+#-p number of threds to use
+#-U fastq file path, and specifies reads are not paired
+#-S file to write SAM alignemnts too (although default is stdout anyhow)
+#-D and -R tell bowtie to try a little harder than normal to find alignments
+#-L reduce substring length to 10 (default 22) as these are short reads
+#-i reduce substring interval? more sensitive?
+#-N max # mismatches in seed alignment; can be 0 or 1 (0)
+#-D give up extending after <int> failed extends in a row (15)
+## this combo of -DRNLi is the same as the preset --very-sensitive (if it takes too long in maize then shorten)
+# -k 2; should tell me multimapping rate??? Consider testing the behaviour of -k, how to get unique mapping reads???
+# -k ...cont... Some blogs say MAPQ is a better filter because "multimapping" is a grey scale not balck and white???
 
 else
 
@@ -110,12 +141,12 @@ bowtie2 \
 -S "$outsam"
 
 # -k report N mapping locations
-#Bowtie 2 does not "find" alignments in any specific order, 
-#so for reads that have more than N distinct, valid alignments, 
-#Bowtie 2 does not guarantee that the N alignments reported are the best possible in terms of alignment score. 
-#Still, this mode can be effective and fast in situations where the user cares more about whether a read aligns 
-#(or aligns a certain number of times) than where exactly it originated.
-fi 
+# Bowtie 2 does not "find" alignments in any specific order,
+# so for reads that have more than N distinct, valid alignments,
+# Bowtie 2 does not guarantee that the N alignments reported are the best possible in terms of alignment score.
+# Still, this mode can be effective and fast in situations where the user cares more about whether a read aligns
+# (or aligns a certain number of times) than where exactly it originated.
+fi
 
 #Using samtools view to convert the sam file to bam file.
 samtools view -S -u $outsam > ${tmpbam}
