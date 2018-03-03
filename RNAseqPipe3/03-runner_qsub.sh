1fe1c1d44b2a25338bcb62a109f64cd337b40df9
pedrocrisp <pedrocrisp@gmail.com>
2018-03-03 14:02:16 -0600

    RNAseqPipe3/03-runner_qsub.sh

---
+++ RNAseqPipe3/03-runner_qsub.sh
@@ -0,0 +1,34 @@
+!/bin/bash -l
+#PBS -l walltime=6:00:00,nodes=1:ppn=12,mem=40gb
+#PBS -N 03-sickle
+#PBS -r n
+#PBS -m abe
+#PBS -M pcrisp@umn.edu
+
+######################
+set -xeuo pipefail
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to PBS_O_WORKDIR
+cd "$PBS_O_WORKDIR"
+echo working dir is now $PWD
+
+mkdir -p logs
+
+######################
+# scythe
+
+module load parallel
+
+bash ~/gitrepos/NGS-pipelines/RNAseqPipe3/03-runner.sh $encoding $threads $read_ends
+
+######################
+# to run eg
+
+# qsub -l walltime=6:00:00,nodes=1:ppn=18,mem=40gb \
+# -v encoding=sanger,threads=12,read_ends=PE \
+# -o logs \
+# -e logs \
+#  ~/gitrepos/NGS-pipelines/RNAseqPipe3/03-runner_qsub.sh
