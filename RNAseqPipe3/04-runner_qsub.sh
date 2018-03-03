e348fd552cddd1c8af56555c6f029104360374d3
pedrocrisp <pedrocrisp@gmail.com>
2018-03-03 15:06:58 -0600

    RNAseqPipe3/04-runner_qsub.sh

---
+++ RNAseqPipe3/04-runner_qsub.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#PBS -N 04-fastqc
+#PBS -r n
+#PBS -m abe
+#PBS -M pcrisp@umn.edu
+
+######################
+set -xeuo pipefail
+
+echo working dir is $PWD
+
+#cd into work dir
+echo changing to PBS_O_WORKDIR
+cd "$PBS_O_WORKDIR"
+echo working dir is now $PWD
+
+mkdir -p logs
+
+######################
+# fastqc post triming
+
+module load parallel
+
+bash ~/gitrepos/NGS-pipelines/RNAseqPipe3/04-runner.sh $threads
+
+######################
+# to run eg
+
+# qsub -l walltime=2:00:00,nodes=1:ppn=18,mem=40gb \
+# -v threads=18 \
+# -o logs \
+# -e logs \
+#  ~/gitrepos/NGS-pipelines/RNAseqPipe3/04-runner_qsub.sh
