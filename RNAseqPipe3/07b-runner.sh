84e50199c1fb2caf363ac7caeee3804388bcc2e1
pedrocrisp <pedrocrisp@gmail.com>
2018-03-14 14:17:06 -0500

    RNAseqPipe3/07b-runner.sh

---
+++ RNAseqPipe3/07b-runner.sh
@@ -0,0 +1,53 @@
+###
+#code to make it work on osx and linux
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+usage="USAGE:
+07b-runner.sh <alignment folder> <number of threads> <strandedness of library> <chromosome.sizes file>"
+
+######### Setup ################
+alignFolder=$1
+threads=$2
+strand=$3
+chromoSizes=$4
+if [ "$#" -lt "3" ]
+then
+echo $usage
+exit -1
+else
+echo "alignment folder = $1, initiating $2 parallel make stranded bigWig jobs, library is $3"
+fi
+########## Run #################
+
+#user defined variables that could be changed:
+workingdir=./
+script=$scriptdir/07b-bam_to_bigwig.sh
+###
+
+function findSamples () {
+find $alignFolder/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
+}
+
+outdir=tdf_for_igv
+mkdir ${outdir}
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+logdir="./logs/${outdir}_subread.${timestamp}"
+mkdir $logdir
+
+cat $script > "$logdir/script.log"
+cat $0 > "$logdir/runner.log"
+cat $script
+
+findSamples | parallel -j $threads bash $script {} $alignFolder $strand $chromoSizes \>logs/${outdir}_subread.${timestamp}/{}.log 2\>\&1
+
+#To run:
+#bash ~/path_to/07-runner.sh
