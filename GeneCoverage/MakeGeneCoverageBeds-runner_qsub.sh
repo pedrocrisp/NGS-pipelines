6d6b436e2aa819a4c8c7fb2d99145fbcce1cbf8f
pedrocrisp <pedrocrisp@gmail.com>
2018-03-21 09:38:50 -0500

    amend paths

--- GeneCoverage/MakeGeneCoverageBeds-runner_qsub.sh
+++ GeneCoverage/MakeGeneCoverageBeds-runner_qsub.sh
@@ -21,7 +21,7 @@ mkdir -p logs
 module load parallel
 module load bedtools/2.25.0
 
-bash ~/gitrepos/NGS-pipelines/RNAseqPipe3/MakeGeneCoverageBeds-runner.sh $alignFolder $threads $reference $coverage
+bash ~/gitrepos/NGS-pipelines/GeneCoverage/MakeGeneCoverageBeds-runner.sh $alignFolder $threads $reference $coverage
 
 ######################
 # to run eg
@@ -31,7 +31,7 @@ bash ~/gitrepos/NGS-pipelines/RNAseqPipe3/MakeGeneCoverageBeds-runner.sh $alignF
 # -v alignFolder=tdf_for_igv,threads=1,reference=~/ws/refseqs/TAIR10/TAIR10_crisp_gene_primary.bed,coverage=full \
 # -o logs \
 # -e logs \
-#  ~/gitrepos/NGS-pipelines/RNAseqPipe3/MakeGeneCoverageBeds-runner_qsub.sh
+#  ~/gitrepos/NGS-pipelines/GeneCoverage/MakeGeneCoverageBeds-runner_qsub.sh
 
 # Make Full coverage beds for IGV viewing and my coverage plots
 # bash ~/gitrepos/NGS-pipelines/GeneCoverage/MakeGeneCoverageBeds-runner.sh tdf_for_igv 1 ~/ws/refseqs/TAIR10/TAIR10_crisp_gene_primary.bed 5prime
