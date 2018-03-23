dec19598138fb42421be735c541e7b87bb10a0af
pedrocrisp <pedrocrisp@gmail.com>
2018-03-23 09:48:30 -0500

    amend \t awk

--- GeneCoverage2/10-CoverageBeds.sh
+++ GeneCoverage2/10-CoverageBeds.sh
@@ -77,8 +77,8 @@ awk -F$'\t' '$NF<1000 && $NF>-1000' $outFolder/$sample.minus.dist.bed > $outFold
 elif [ "$coverage" == "full" ]
 then
 #make a real bed file by adding chromosome end co-ordinates (same as start because nt resolution)
-awk 'BEGIN { FS="\t"; OFS="\t" } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.plus.bed > $outFolder/$sample.plus.real.bed
-awk 'BEGIN { FS="\t"; OFS="\t" } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.minus.bed > $outFolder/$sample.minus.real.bed
+awk -F$"\\t" 'BEGIN { OFS = FS } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.plus.bed > $outFolder/$sample.plus.real.bed
+awk -F$"\\t" 'BEGIN { OFS = FS } { $2=$2 "\t" $2 } 1' $sample_dir/$sample.minus.bed > $outFolder/$sample.minus.real.bed
 
 #sort
 sort -k1,1 -k2,2n $outFolder/$sample.plus.real.bed > $outFolder/$sample.plus_sorted.bed
